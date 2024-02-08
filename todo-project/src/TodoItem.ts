c94b12999d7acf25bcbd378f20f4d1914cca2ce8
Lee Garam <agerio100@naver.com>
2024-02-08 22:05:14 +0900

    Typescript annotations
    classes, class properties, imports and exports, and types

---
+++ todo-project/src/TodoItem.ts
@@ -0,0 +1,14 @@
+class TodoItem {
+    // Build the constructor with the properties of the class
+    constructor(public id : number, public task : string, public isCompleted : boolean) {
+        this.id = id;
+        this.task = task;
+        this.isCompleted = isCompleted;
+    }
+
+    printDetails() : void {
+        console.log(`${this.id}\t${this.task}\t${this.isCompleted ? '(completed)' : ''}`)
+    }
+}
+
+export default TodoItem; // Export the class to be used in other files
--- todo-project/src/index.ts
+++ todo-project/src/index.ts
@@ -1,3 +1,12 @@
-console.log('Hello, TypeScript!')
-console.log('Build test')
-console.log('Another test')
+import TodoItem from "./TodoItem";
+
+const data = [
+    {id : 1, task : 'Buy Milk', isCompleted : true},
+    {id : 2, task : 'Pay Bills', isCompleted : false},
+]
+
+console.log('My Todo List');
+for (let i = 0; i < data.length; i++) {
+    let todoItem = new TodoItem(data[i].id, data[i].task, data[i].isCompleted);
+    todoItem.printDetails();
+}
